76841d80d81bbd493b1e2e5929daf699139731a4
Alfredo Ramos <50826496+alfredoramosgarcia@users.noreply.github.com>
2023-05-10 12:08:23 +0200

    Primeros avances FPS

---
+++ eChapasUnity/Assets/Scenes/Campo/Scripts/MoverChapa.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MoverChapa : MonoBehaviour
+{
+    public GameObject chapa; // La chapa que se moverá
+    public float fuerza = 10.0f; // La fuerza con la que se lanzará la chapa
+
+    private Rigidbody chapaRigidbody; // Referencia al RigidBody de la chapa
+
+    private void Start()
+    {
+        chapaRigidbody = chapa.GetComponent<Rigidbody>(); // Obtener la referencia del RigidBody de la chapa
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Si se presiona el botón izquierdo del ratón (0 es el código para el botón izquierdo)
+        {
+            Vector3 direccion = transform.forward; // Obtener la dirección en la que está apuntando el FPSController
+
+            chapaRigidbody.AddForce(direccion * fuerza, ForceMode.Impulse); // Aplicar una fuerza a la chapa en la dirección del FPSController
+        }
+    }
+}
+
