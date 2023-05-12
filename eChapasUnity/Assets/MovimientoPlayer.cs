b56d70dae282c813915b2682f76910c6cab66d41
Alfredo Ramos <50826496+alfredoramosgarcia@users.noreply.github.com>
2023-05-12 19:16:23 +0200

    b

--- eChapasUnity/Assets/MovimientoPlayer.cs
+++ eChapasUnity/Assets/MovimientoPlayer.cs
@@ -16,7 +16,11 @@ public class MoverPlayer : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) // Si se presiona el botón izquierdo del ratón (0 es el código para el botón izquierdo)
         {
+<<<<<<< HEAD:eChapasUnity/Assets/MovimientoPlayer.cs
             Vector3 direccion = (Camera.main.transform.position - transform.position).normalized;
+=======
+            Vector3 direccion = transform.forward; // Obtener la dirección en la que está apuntando el FPSController
+>>>>>>> parent of b1cc2bb (he):eChapasUnity/Assets/Scenes/Campo/Scripts/MoverChapa.cs
 
             chapaRigidbody.AddForce(direccion * fuerza, ForceMode.Impulse); // Aplicar una fuerza a la chapa en la dirección del FPSController
         }
