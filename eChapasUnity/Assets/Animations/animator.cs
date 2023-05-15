cb04ef6388d55de9be8bd7014d645792bbc63531
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2023-05-16 00:30:08 +0200

    [Features] Controlador de animaciones

---
+++ eChapasUnity/Assets/Animations/animator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animator : MonoBehaviour
+{
+
+    private Animator anim;
+    
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
